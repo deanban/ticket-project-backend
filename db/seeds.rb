c88fb39b59f6292bb4869eea5599787bb76c3d1b
dkush22 <dkush22@gmail.com>
2017-09-13 11:22:02 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,12 @@
 Event.create([
 {name: 'Bruno Mars', event_type: 'concert', state: 'NY', zip: 11021, price: 100},
+{name: 'New York Jets vs. New England Patriots', event_type: 'football', state: 'NY', zip: 11021, price: 100},
+{name: 'Bon Jovi', event_type: 'concert', state: 'NY', zip: 11021, price: 100},
+{name: 'Flatiron Presents', event_type: 'concert', state: 'NY', zip: 11021, price: 100},
+{name: 'New York Yankees vs. Boston Red Sox', event_type: 'baseball', state: 'NY', zip: 10451, price: 200},
+{name: 'Miami Dolphins vs. Buffalo Bills', event_type: 'football', state: 'FL', zip: 48104, price: 150},
+{name: 'Carolina Panthers vs. Atlanta Falcons', event_type: 'football', state: 'NC', zip: 48104, price: 150},
+{name: 'Atlanta Falcons vs. New Orleans Saints', event_type: 'football', state: 'GA', zip: 48104, price: 150},
+{name: 'Detroit Tigers vs. Kansas City Royals', event_type: 'football', state: 'MI', zip: 48104, price: 150},
 {name: 'Detroit Lions vs. Green Bay Packers', event_type: 'football', state: 'MI', zip: 48104, price: 150},
-{name: 'New York Yankees vs. Boston Red Sox', event_type: 'baseball', state: 'NY', zip: 10451, price: 200}
 ])
